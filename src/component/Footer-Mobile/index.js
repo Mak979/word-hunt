2bb84bff2d817cce6b823943cd86c3c9764ad815
Mak979 <iammayank777@gmail.com>
2021-09-26 14:40:06 +0530

    Feature Added: Bookmarks

--- src/component/Footer-Mobile/index.js
+++ src/component/Footer-Mobile/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import { useState } from 'react/cjs/react.development'
+import Bookmarks from '../Bookmarks'
 
 const MobileFooter = () => {
+    const [showBookmarks, setShowBookmarks] = useState(false)
     return (
-        <div className="fixed bottom-0 w-full md:hidden p-2.5 flex justify-evenly bg-gray-300 rounded-t-lg shadow-2xl">
-            <div className="w-10 h-10 flex justify-center align-center pt-1.5 rounded-full bg-indigo-300"><i className="fas fa-bookmark text-indigo-900 cursor-pointer text-xl" title="Bookmarks" ></i></div>
-            <div className="w-10 h-10 flex justify-center align-center pt-1.5 rounded-full bg-indigo-300"><i className="fas fa-clipboard-list text-indigo-900 cursor-pointer text-xl" title="History" ></i></div>
+        <>
+        <div className="fixed bottom-0 w-full md:hidden p-2.5 flex justify-evenly bg-indigo-100 rounded-t-3xl shadow-2xl">
+            <div onClick={() => setShowBookmarks(true)} className="w-10 h-10 flex justify-center align-center pt-1.5 rounded-full bg-gray-200"><i className="fas fa-bookmark text-indigo-900 cursor-pointer text-xl" title="Bookmarks" ></i></div>
+            <div className="w-10 h-10 flex justify-center align-center pt-1.5 rounded-full bg-gray-200"><i className="fas fa-clipboard-list text-indigo-900 cursor-pointer text-xl" title="History" ></i></div>
             
         </div>
+        {
+            showBookmarks && <Bookmarks show={showBookmarks} close={() => setShowBookmarks(false)}/>
+        }
+        </>
     )
 }
 
--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -1,20 +1,35 @@
 import React, { useState } from 'react'
 
 const Header = ({word, phonetic, src}) => {
+    let bookmarks = []
+    let str = localStorage.getItem('Bookmarks')
+    let localBookmark = str.split(',')
+    // if(str.length > 1) {
+    //     localBookmark = str.split(',')
+    // }
+    bookmarks.push(localStorage.getItem('Bookmarks'))
     const [showAudio, setShowAudio] = useState(false)
     const [bookmarked, setBookmarked] = useState(false)
     const handleAudio = () => {
         showAudio ? setShowAudio(false) : setShowAudio(true)
     }
     const handleBookmarkClick = () => {
-        console.log('clicked');
+        if(localBookmark.includes(word)) {
+            let index = localBookmark.indexOf(word)
+            localBookmark.splice(index, 1)            
+            localStorage.setItem('Bookmarks', localBookmark)
+        } else {
+            bookmarks.push(word)
+            localStorage.setItem('Bookmarks', bookmarks)
+        }
+        
         bookmarked ? setBookmarked(false) : setBookmarked(true)
     }
     return (
         <>
             <div className="w-full flex justify-center text-4xl md:text-8xl text-center text-gray-300 tracking-wider uppercase mb-4 md:mb-8">
                 <div className="px-3 overflow-ellipsis overflow-hidden"><h1 className="text-center">{word}</h1></div>
-                {phonetic && <div className="w-1/12 -mt-4 md:-mt-10"><i onClick={handleBookmarkClick} className={`${bookmarked ? 'fas' : 'far'} fa-bookmark cursor-pointer text-2xl md:text-4xl text-center ml-4`} title="Add to Bookmarks" ></i></div>}
+                {phonetic && <div className="w-1/12 -mt-4 md:-mt-10"><i onClick={handleBookmarkClick} className={`${localBookmark?.includes(word) ? 'fas' : 'far'} fa-bookmark cursor-pointer text-2xl md:text-4xl text-center ml-4`} title="Add to Bookmarks" ></i></div>}
             </div>
             { phonetic && 
                 <div className="text-center">
