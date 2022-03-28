be7a466a4f4d65f1720ed7a53a5ba6b21153642e
Mak979 <iammayank777@gmail.com>
2022-03-28 20:07:31 +0530

    Design changes related to bookmarks component - Added scroll behaviour and fixed the bookmark heading to top

--- src/component/Bookmarks/index.js
+++ src/component/Bookmarks/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import MobileFooter from "../Footer-Mobile";
 
 const Bookmarks = ({ show, close, bookmarkClick }) => {
   let str;
@@ -18,24 +19,26 @@ const Bookmarks = ({ show, close, bookmarkClick }) => {
       <div
         className={`${
           show ? "block" : "hidden"
-        } lg:hidden fixed top-0 left-0 w-full min-h-screen h-auto z-20 bg-gradient-to-b from-indigo-500 to-purple-400`}
+        } lg:hidden absolute top-0 left-0 min-h-screen overflow-hidden z-20 bg-gradient-to-b from-indigo-500 to-purple-400`}
       >
-        <div className="p-2 w-full mb-8">
-          <i 
-            onClick={() => close()}
-            className="fas fa-arrow-left absolute left-5 top-4 text-indigo-900 cursor-pointer text-2xl"
-          />
-          <div className="w-4/5 mx-auto text-center text-4xl text-indigo-900">
+        <div className="fixed top-0 left-0 z-10 p-2 w-full mb-8 bg-indigo-500 shadow-lg">
+          <div className="float-left w-10 h-10 rounded-full hover:bg-gray-200">
+            <i 
+              onClick={() => close()}
+              className="fas fa-arrow-left absolute left-5 top-4 text-indigo-900 cursor-pointer text-xl"
+            />
+          </div>
+          <div className="w-4/5 mx-auto text-center text-3xl text-indigo-900">
             Bookmarks
           </div>
         </div>
 
         {bookmarks && bookmarks.length ? (
-          <div className="w-full p-4 text-xl text-indigo-900 flex flex-wrap">
+          <div className="w-full mb-12 p-4 mt-12 text-xl text-indigo-900 flex flex-wrap">
             {bookmarks.map((item, index) => {
               return (
                 item !== "" && (
-                  <div className="w-auto flex justify-between border-2 border-white rounded-md p-2 cursor-pointer mr-6 mb-4">
+                  <div className="w-auto flex justify-between border-2 border-white rounded-md p-2 cursor-pointer mr-5 mb-4">
                     <div
                       onClick={() => handleBookmarkClick(item)}
                       key={index}
@@ -50,11 +53,12 @@ const Bookmarks = ({ show, close, bookmarkClick }) => {
             })}
           </div>
         ) : (
-          <div className="mt-36 w-full text-center text-2xl text-indigo-900 p-4">
+          <div className="mt-72 w-full text-center text-2xl text-indigo-900 p-4">
             Sorry Pal, No Bookmarks added. Let's add one to get you started.
           </div>
         )}
       </div>
+      <MobileFooter />
     </>
   );
 };
